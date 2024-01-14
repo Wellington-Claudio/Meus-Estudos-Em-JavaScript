1510db0833a1e6302edc566683998e2383fefa2e
Wellington Santos <wellingthon360@gmail.com>
2024-01-14 19:02:23 -0300

    lçhnklçnlç
    klçklçklç

--- ex014/script.js
+++ ex014/script.js
@@ -1,8 +1,1 @@
-function carregar(){
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    data = new Date()
-    var hora = data.getHoures()
-    msg.innerHTML = `Agora são ${hora} horas.`
-
-}
+alert ('Olá');
