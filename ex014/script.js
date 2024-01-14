f978b3becb8a0082a3cc757681dc224a3f0cba7e
Wellington Santos <wellingthon360@gmail.com>
2024-01-14 19:35:12 -0300

    Final do primeiro exercício

--- ex014/script.js
+++ ex014/script.js
@@ -1,1 +1,21 @@
-alert ('Olá');
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 2
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        //bom dia!
+        img.src = 'manha-cortada-redimencionada.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >=12 && hora < 18) {
+        //boa tarde! 
+        img.src = 'tarde-cortada-redimencionada.jpg'
+        document.body.style.background = '#b9846f'
+    } else {
+        //boa noite!
+        img.src = 'noite-cortada-redimencionada.jpg'
+        document.body.style.background = '#515154'
+    }
+}
