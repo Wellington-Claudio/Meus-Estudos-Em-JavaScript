f4ffa7daa4b29597dc47103d56210b61cb95cc97
Wellington Santos <wellingthon360@gmail.com>
2024-01-14 20:48:49 -0300

    Início do esqueleto, segundo exercício

--- ex015/script.js
+++ ex015/script.js
@@ -1,1 +1,19 @@
-alert ('Olá');
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verificar os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        if (fsex[0].checked) {
+            genero = 'homem'
+        } else if (fsex[1].checked) {
+            genero = 'mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    }
+}
