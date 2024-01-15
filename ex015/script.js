d2b4985cf23f7abfc392ce45b8aca428d225d191
Wellington Santos <wellingthon360@gmail.com>
2024-01-15 18:29:32 -0300

    fim dos primeiros exercícios

--- ex015/script.js
+++ ex015/script.js
@@ -15,27 +15,35 @@ function verificar() {
             genero = 'homem'
             if (idade >=0 && idade <10) {
                 //criança
-                img.setAttribute('srd', '')
+                img.setAttribute('src', 'bebe menino-redimencionado.jpg')
             } else if (idade >= 10 && idade < 21) {
                 //jovem
+                img.setAttribute('srd', 'criança menino-redimencionado.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'adulto-redimencionado.jpg')
             } else {
                 //idoso
+                img.setAttribute('src', 'idoso-redimencionado.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'mulher'
             if (idade >=0 && idade <10) {
                 //criança
+                img.setAttribute('src', 'bebe menina-redimencionado.jpg')
             } else if (idade >= 10 && idade < 21) {
                 //jovem
+                img.setAttribute('src', 'criana menina-redimencionado.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'adulta-redimencionado.jpg')
             } else {
                 //idoso
+                img.setAttribute('src', 'idosa-redimencionado.jpg')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
